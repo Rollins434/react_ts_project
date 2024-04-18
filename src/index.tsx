f7580a06605c93952cf7c0ddc6738cdd02f2235d
Rupendra Aathiya <rupendra.aathiya@marutitech.com>
2024-04-18 23:57:05 +0530

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import ReactDom from "react-dom";
-// import Parent from "./props/Parent";
+
 import "../src/styles/global.css"
-// import GuestList from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+
 
 const App = () => {
   return (
     <div>
-        {/* <GuestList/> */}
-        <UserSearch/>
+       hi
     </div>
   );
 };
